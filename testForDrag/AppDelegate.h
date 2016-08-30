0837f493c11f65800e6d5dacf33050c768e0094c
lisuping <lisuping@lukou.com>
2016-08-30 16:01:07 +0800

    Initial Commit

---
+++ testForDrag/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testForDrag
+//
+//  Created by SupingLi on 16/8/30.
+//  Copyright © 2016年 SupingLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testForDrag/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testForDrag
+//
+//  Created by SupingLi on 16/8/30.
+//  Copyright © 2016年 SupingLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
